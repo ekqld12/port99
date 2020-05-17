f3d5c1f12880605a27d0b9bc5274f6e4559f368f
ekqld12 <65323819+ekqld12@users.noreply.github.com>
2020-05-17 23:18:41 +0900

    2020.05.17
    농심 서브페이지

--- nongshim/sub/assets/js/costom.js
+++ nongshim/sub/assets/js/costom.js
@@ -1,32 +1,14 @@
 var mySwiper = new Swiper ('#banner .swiper-container', {
-    // Optional parameters
-    loop: true,
+  // Optional parameters
+  loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 3,
+      centeredSlides: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: 'true',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 5000,
-      }
-  })
-
-  var mySwiper2 = new Swiper ('.notice .swiper-container', {
-    loop: true,
-    spaceBetween: 60,
-    slidesPerView: 'auto',
-    centeredSlides: true
-  })
-
-  var myswiper3 = new Swiper('.story .swiper-container', {
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: true
-  });
+  // If we need pagination
+  pagination: {
+    el: '#nlive-swiper .swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+},
+});

dec38f836008f040b3bcebbb031f98c681ee04a9
ekqld12 <65323819+ekqld12@users.noreply.github.com>
2020-05-19 20:54:06 +0900

    농심 서브페이지
    2020.05.19

---
+++ nongshim/sub1/assets/js/costom.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper ('#banner .swiper-container', {
+  // Optional parameters
+  loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 3,
+      centeredSlides: true,
+
+  // If we need pagination
+  pagination: {
+    el: '#banner .swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+},
+});

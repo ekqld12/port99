2997d21e608cd0c15a5cf85417c0624f22a0a652
ekqld12 <65323819+ekqld12@users.noreply.github.com>
2020-06-30 11:31:10 +0900

    2020.06.30
    수정

---
+++ port/assets/js/costom.js
@@ -0,0 +1,13 @@
+//var mySwiper3 = new Swiper('#img-slide .swiper-container', {
+//    loop: true, 
+//    el: '.swiper-scrollbar',
+//    slidesPerView: 'auto',
+//    centeredSlides: true,
+//})
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    hide: false,
+  },
+});
